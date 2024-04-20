ba1fe1c957293a349be023c06a966ba96fbe1c34
Denis Kaliuzhnyi <deniskl4200@gmail.com>
2024-04-20 16:30:31 +0300

    3

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,27 +1,23 @@
-const fetchImages = imageName => {
-  const params = new URLSearchParams({
-    key: '43257853-194068c59ee252fa44b7d008e',
-    q: imageName,
-    image_type: 'photo',
-    orientation: 'horizontal',
-    safesearch: true,
-  });
+import axios from 'axios';
 
-  return fetch(`https://pixabay.com/api/?${params}`, {
-    header: {
-      'Access-Control-Allow-Origin': 'https://pixabay.com',
-    },
-  })
-    .then(res => {
-      if (!res.ok) {
-        throw new Error(res.status);
-      }
-      return res.json();
-    })
-    .then(data => {
-      return data.hits;
-    })
-    .catch(error => console.log(error));
+const fetchImages = async (imageName, page = 1, perPage = 15) => {
+  try {
+    const response = await axios.get('https://pixabay.com/api/', {
+      params: {
+        key: '43257853-194068c59ee252fa44b7d008e',
+        q: imageName,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+        page,
+        per_page: perPage,
+      },
+    });
+    return response.data.hits;
+  } catch (error) {
+    console.error('Error fetching images:', error);
+    throw new Error('Failed to fetch images. Please try again later');
+  }
 };
 
 export { fetchImages };
